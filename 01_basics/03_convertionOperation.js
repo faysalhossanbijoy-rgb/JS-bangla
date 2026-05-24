3318860fa1841e14d7aa17e277dbe851cde6e73e
faysalhossanbijoy-rgb <faysalhossanbijoy@gmail.com>
2026-05-24 19:16:07 +0000

    operations

--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -1,12 +1,27 @@
 let goal = "4"
 
 
-console.log(typeof goal)
+//console.log(typeof goal)
 
 
 let valuenew = Number(goal)
 
 
-console.log(typeof goal)
+//console.log(typeof goal)
 
 // in javascript we can convert type of data
+// Operations in javascript
+
+let value = 3
+let negvalue = -value
+
+
+//console.log(negvalue);
+//console.log();
+
+let str1 = "hello Bangladesh"
+let str2 = " this is me and dream"
+
+
+let str3 = str1 + str2
+console.log(str3);
